7cfe9c6cb75545227b6e0198daceec313837fad0
Abdullah <abd00s@users.noreply.github.com>
2015-02-24 17:40:00 -0500

    Add DB seeds to test associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Project.destroy_all
+Activity.destroy_all
+Dependency.destroy_all
+p = Project.create(name: "Pilot")
+a = p.activities.create(name:"Excavation", amount: 100, rate: 10, crew_size: 5)
+b = p.activities.create(name:"Earthwork", amount: 100, rate: 10, crew_size: 5)
+c = p.activities.create(name:"Foundations", amount: 100, rate: 10, crew_size: 5)
+Dependency.create([{ activity_id: b.id, predecessor_id: a.id}, { activity_id: c.id, predecessor_id: a.id }, { activity_id: c.id, predecessor_id: b.id }])
+
