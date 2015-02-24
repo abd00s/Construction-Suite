0c7d1d8cd3d95e4a166724cad11d1881e3260274
Abdullah <abd00s@users.noreply.github.com>
2015-02-24 16:15:14 -0500

    G Dependecy model to be act as self join, and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224183921) do
+ActiveRecord::Schema.define(version: 20150224211342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20150224183921) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+  end
+
+  create_table "dependencies", force: true do |t|
+    t.integer  "activity_id"
+    t.integer  "dependent_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "projects", force: true do |t|
