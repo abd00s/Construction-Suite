47dcb37b75e916f331c1330989bc37f75ff09989
Abdullah <abd00s@users.noreply.github.com>
2015-02-24 13:12:20 -0500

    Add Activity presence of all attrs validation and first spec

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Activity, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Activity do
+  let!(:activity) { Activity.create }
+  it  "Must have all attributes" do 
+    expect(activity).to_not be_valid
+  end
 end
