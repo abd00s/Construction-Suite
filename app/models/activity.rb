e1141527f1623010ab662f7b0005e22c23c2f06f
Abdullah <abd00s@users.noreply.github.com>
2015-02-24 13:25:35 -0500

    Spec test association of activity to project

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,4 +2,5 @@ class Activity < ActiveRecord::Base
   include ManhourEstimator
   belongs_to :project
   validates :name, :amount, :rate, :crew_size, :morning_crews, :evening_crews, :project_id, presence: true
+  validates_associated :project
 end
--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
 
 describe Activity do
-  let!(:activity) { Activity.create }
-  it  "Must have all attributes" do 
+  it  "Must have all attributes" do
+    activity = Activity.create 
     expect(activity).to_not be_valid
+    activity = Activity.create(name: "Test", rate: 10, amount: 100, crew_size: 5, morning_crews: 2, evening_crews: 2, project_id: 1)
+    expect(activity).to be_valid    
   end
+
+  it  "Must be associated to a Project" do
+    project = Project.create(name: "Pilot", week_length: 5, shift_length: 10)
+    activity = project.activities.build(name: "Test", rate: 10, amount: 100, crew_size: 5, morning_crews: 2, evening_crews: 2) 
+    expect(activity).to be_valid
+    expect(activity.project).to eq(project)
+  end  
 end
