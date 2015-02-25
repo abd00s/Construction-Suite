c5ece32348ace27f394d0b3b5a2be8ad6242fde0
Abdullah <abd00s@users.noreply.github.com>
2015-02-25 13:16:41 -0500

    Bug fix, switch successors call with predecessors

--- app/models/pathset.rb
+++ app/models/pathset.rb
@@ -1,18 +1,17 @@
 module Pathset 
-  $vertices = {}
   def method
-    $prevertex_activities = activities.all.to_a
-    key = $vertices.count + 1
+    @vertices = {}
+    @prevertex_activities ||= activities.all.to_a
+    key = @vertices.count + 1
     activities.all.each_with_index{|activity, index|
-      if activity.successors.count == 0
-        $vertices[key] ||= []
-        $vertices[key] << activity
+      if activity.predecessors.count == 0
+        @vertices[key] ||= []
+        @vertices[key] << activity
       end
-      # puts activity.name
-      # puts index
-      # puts "__"
     }    
-    $prevertex_activities -= $vertices[key]
-    print $prevertex_activities
+    @prevertex_activities -= @vertices[key]
+    print @prevertex_activities
+    puts " ____ "
+    print @vertices
   end
 end
