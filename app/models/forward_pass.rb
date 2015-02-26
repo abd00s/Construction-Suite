04549b411433797440963a2501fe4099633c4716
Abdullah <abd00s@users.noreply.github.com>
2015-02-26 18:10:57 -0500

    Successfully target activities in subsequent vertices (i.e. not Vertex 1)

--- app/models/forward_pass.rb
+++ app/models/forward_pass.rb
@@ -5,5 +5,16 @@ module ForwardPass
       activity.early_finish = activity.duration
       activity.save
     }
+    set_remaining_vertices(activities) if activities[2]
+  end
+
+  def set_remaining_vertices(vertices)
+    vertices.each_with_index{|(vertex,activities), index|
+      unless index == 0
+        vertices[vertex].each{|activity|
+          puts activity.name
+        }
+      end
+    }
   end
 end
