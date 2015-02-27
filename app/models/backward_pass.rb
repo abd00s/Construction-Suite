c106072edfeb3dbd9806ccb6dada05a916a9c9c8
Abdullah <abd00s@users.noreply.github.com>
2015-02-27 16:35:22 -0500

    Backward Pass sets LF,LS successfully (git add -u)

--- app/models/backward_pass.rb
+++ app/models/backward_pass.rb
@@ -5,6 +5,17 @@ module BackwardPass
   end
 
   def set_lates(activities)
-    
+    activities.each{|vertex,contents|
+      contents.each{|activity|
+        activity.late_finish = if activity.successors.count == 0
+          activity.early_finish >= self.activities.maximum(:early_finish) ? activity.early_finish : self.activities.maximum(:early_finish)
+        else
+          activity.successors.minimum(:late_start)
+        end
+        activity.save
+        activity.late_start = activity.late_finish - activity.duration
+        activity.save
+      }
+    }
   end
 end
