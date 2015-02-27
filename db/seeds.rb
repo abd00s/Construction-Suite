f66e26500e95323b9154d87f14a904b4eee8a0f6
Abdullah <abd00s@users.noreply.github.com>
2015-02-27 15:47:30 -0500

    Modify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ d = p.activities.create(name:"Foundations", amount: 100, rate: 10, crew_size: 5,
 e = p.activities.create(name:"Electric", amount: 100, rate: 10, crew_size: 5, duration: 7)
 f = p.activities.create(name:"Piping", amount: 100, rate: 10, crew_size: 5, duration: 9)
 g = p.activities.create(name:"Mechanical", amount: 100, rate: 10, crew_size: 5, duration: 4)
-h = p.activities.create(name:"HVAC", amount: 100, rate: 10, crew_size: 6)
+h = p.activities.create(name:"HVAC", amount: 100, rate: 10, crew_size: 6, duration: 5)
 i = p.activities.create(name:"Utilities", amount: 100, rate: 10, crew_size: 5, duration: 8)
 Dependency.create([{ activity_id: b.id, predecessor_id: a.id}, 
                   { activity_id: c.id, predecessor_id: a.id }, 
@@ -30,3 +30,14 @@ Dependency.create([{ activity_id: b.id, predecessor_id: a.id},
                   { activity_id: i.id, predecessor_id: h.id },
                   ])
 
+x = Project.create(name: "Syque")
+l = x.activities.create(name:"VCO", amount: 100, rate: 10, crew_size: 5, duration: 1)
+m = x.activities.create(name:"AS", amount: 100, rate: 10, crew_size: 5, duration: 2)
+n = x.activities.create(name:"EC", amount: 100, rate: 10, crew_size: 5, duration: 3)
+o = x.activities.create(name:"RS", amount: 100, rate: 10, crew_size: 5, duration: 5)
+p = x.activities.create(name:"VS", amount: 100, rate: 10, crew_size: 5, duration: 1)
+Dependency.create([{ activity_id: n.id, predecessor_id: l.id}, 
+                  { activity_id: n.id, predecessor_id: m.id }, 
+                  { activity_id: o.id, predecessor_id: m.id },
+                  { activity_id: p.id, predecessor_id: n.id }
+                  ])
