f442272671b80c5f5391e94c6da44e07c55b7a69
Abdullah <abd00s@users.noreply.github.com>
2015-02-27 17:16:05 -0500

    Successfully set Activity Total FLoat

--- app/models/set_floats.rb
+++ app/models/set_floats.rb
@@ -1,5 +1,14 @@
 module SetFloats
   def set_floats(activities)
-    puts activities
+    activities.each_value do |vertex|
+      vertex.each do |activity|
+        set_total_float(activity)
+      end
+    end
+  end
+
+  def set_total_float(activity) 
+    activity.total_float = activity.late_start - activity.early_start
+    activity.save
   end
 end
